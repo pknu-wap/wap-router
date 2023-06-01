1be212bc9cfc0c3d503665b639e07f0a9495c09d
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-01 19:12:27 +0900

    chore: export default as usePath

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useNavigate } from './useNavigate';
 export { default as useParams } from './useParams';
 export { default as useCurrentRoute } from './useCurrentRoute';
+export { default as usePath } from './usePath';
